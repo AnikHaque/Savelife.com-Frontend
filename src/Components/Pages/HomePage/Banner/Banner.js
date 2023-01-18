699a7b7ce560938b35b9003c939b7a004e54289e
AnikHaque <ekramul15-811@diu.edu.bd>
2023-01-19 00:46:08 +0600

    banner added

---
+++ src/Components/Pages/HomePage/Banner/Banner.js
@@ -0,0 +1,17 @@
+// configuration 
+import React from 'react'
+
+// css files 
+import './Banner.css'
+
+ const Banner = () => {
+  return (
+    <div>
+        {/* background  */}
+        <div className='banner-bg'>
+
+        </div>
+    </div>
+  )
+}
+export default Banner;
